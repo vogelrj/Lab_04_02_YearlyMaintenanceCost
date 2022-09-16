d10c68e3e4662376e44246e446d40c102848bdf0
Rudi <vogelrj@mail.uc.edu>
2022-09-15 21:15:38 -0400

    Initial commit

---
+++ src/Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,14 @@
+public class Lab_04_02_YearlyMaintenanceCost {
+    public static void main(String[] args) {
+    double winterMaint = 251.46;
+    double springMaint = 164.75;
+    double summerMaint = 748.14;
+    double fallMaint = 349.47;
+    double totalMaint = winterMaint + springMaint + summerMaint + fallMaint;
+        System.out.println("Your winter home maintenance cost is $" + winterMaint);
+        System.out.println("Your spring home maintenance cost is $" + springMaint);
+        System.out.println("Your summer home maintenance cost is $" + summerMaint);
+        System.out.println("your summer home maintenance cost is $" + fallMaint);
+        System.out.println("Your total home maintenance cost for the year is $" + totalMaint);
+    }
+}
